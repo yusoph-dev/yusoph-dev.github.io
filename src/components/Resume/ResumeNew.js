8a43512e36e92501e789ebc147ce7ba2e749ffef
damascus08 <yusophh@gmail.com>
2024-05-29 12:42:23 +0800

    update:

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -33,10 +33,10 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 786 ? 1.8 : 0.6} />
           </Document>
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.8 : 0.6} />
           </Document>
         </Row>
 
