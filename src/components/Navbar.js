3bd21ee0ea9a21afb174a4dfa0835aff37694cae
yusoph-umbra <161783161+yusoph-umbra@users.noreply.github.com>
2025-03-13 03:15:43 +0800

    feature: logs for site visitors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-// filepath: /Applications/XAMPP/xamppfiles/htdocs/GitHub/Portfolio-yusoph/src/components/Navbar.js
 import React, { useEffect, useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -7,21 +6,15 @@ import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-// import { ImBlog } from "react-icons/im";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
-
+import { AiFillStar, AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from "react-icons/ai";
 import { CgFileDocument } from "react-icons/cg";
 import { db } from "../firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
   const [navColour, updateNavbar] = useState(false);
+  const [visitorCount, setVisitorCount] = useState(0);
 
   useEffect(() => {
     const token = process.env.REACT_APP_IPINFO_TOKEN;
@@ -41,6 +34,15 @@ function NavBar() {
         try {
           await addDoc(collection(db, "visitorLogs"), logEntry);
         } catch (e) {
+          console.error("Error adding log entry to Firestore: ", e);
+        }
+
+        // Fetch visitor count from Firestore
+        try {
+          const querySnapshot = await getDocs(collection(db, "visitorLogs"));
+          setVisitorCount(querySnapshot.size);
+        } catch (e) {
+          console.error("Error fetching visitor count from Firestore: ", e);
         }
       })
       .catch(error => console.error('Error fetching visitor info:', error));
@@ -139,7 +141,9 @@ function NavBar() {
               </Button>
             </Nav.Item>
             <Nav.Item>
-              
+              <Nav.Link>
+                <AiOutlineUser style={{ marginBottom: "2px" }} /> {visitorCount}
+              </Nav.Link>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
